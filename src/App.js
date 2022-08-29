32bd8386b227d138ab7fef269040f4bb28514da9
cre8ivesolutions <tammerz78@gmail.com>
2022-08-29 18:53:53 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,53 +1,58 @@
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
-
-import './App.css';
-
-import Home from './components/Home'
-import About from './components/About'
-import Packages from './components/Packages'
-
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Packages from "./components/Packages";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
 function App() {
-  
-  const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
-
+  const packages = [
+    "Activate your Crystals",
+    "Monkey Meditation",
+    "Soak in the Hotsprings",
+    "Hypnotherapy",
+    "Mineral Bath",
+  ];
 
   return (
     <div className="App">
       <Router>
         <header>
           <h1 className="title">Welcome to Monty's Mineral SPA</h1>
-
           <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/packages">Our Packages</Link>
-              </li>
-            </ul>
+            <Container>
+              <Nav defaultActiveKey="/" variant="tabs" fill>
+                <Nav.Link href="/">
+                  <Nav.Item>
+                    <Link to="/">Home</Link>
+                  </Nav.Item>
+                </Nav.Link>
+                <Nav.Link eventKey={"aboutPage"}>
+                  <Nav.Item>
+                    <Link to="/about">About Us</Link>
+                  </Nav.Item>
+                </Nav.Link>
+                <Nav.Link eventKey={"packagesPage"}>
+                  <Nav.Item>
+                    <Link to="/packages">Our Packages</Link>
+                  </Nav.Item>
+                </Nav.Link>
+              </Nav>
+            </Container>
           </div>
         </header>
-
         <div className="display">
           <Route path="/" component={Home} />
           <Route path="/about" component={About} />
-          <Route path="/packages" render={() => <Packages packages={packages}/>}  />
+          <Route
+            path="/packages"
+            render={() => <Packages packages={packages} />}
+          />
         </div>
-
       </Router>
-
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,18 +1,26 @@
-
+import ListGroup from "react-bootstrap/ListGroup";
+import Container from "react-bootstrap/Container";
 
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
+  const displayPackages = props.packages.map((eachPackage) => (
+    <li>{eachPackage}</li>
+  ));
 
-    return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
-                    {displayPackages}
-                </ul>
-            </div>
+  return (
+    <div>
+      <div className="packages">
+        <div className="packagesHeader">
+          <h1>Our Packages</h1>
         </div>
-    )
+        <Container>
+          <ListGroup >
+            {/* <ListGroup.Item active></ListGroup.Item> */}
+            <ListGroup.Item className="packageList">{displayPackages}</ListGroup.Item>
+          </ListGroup>
+        </Container>
+        {/* <ul>
+                </ul> */}
+      </div>
+    </div>
+  );
 }
